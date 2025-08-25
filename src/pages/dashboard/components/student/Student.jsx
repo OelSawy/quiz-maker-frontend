c21cb34be48160cb922b990457e012fdb34b0ecf
Omar Abdelwahab <Maroabdelwahab@gmail.com>
2025-08-25 09:05:21 +0300

    feat: add student dashboard and relevant components

--- src/pages/dashboard/components/student/Student.jsx
+++ src/pages/dashboard/components/student/Student.jsx
@@ -1,3 +1,43 @@
+// @ts-nocheck
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import QuizItem from './components/QuizItem';
+import QuizList from './components/QuizList';
+import { getQuizzesStudent } from '../../../../redux/reducers/studentSlicer';
+
 export default function Student() {
-  return <div>student</div>;
+  const {
+    getQuizzesStatus,
+    submitQuizStatus,
+    error,
+    quizzes,
+    answers,
+  } = useSelector(state => state.student);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+      dispatch(getQuizzesStudent(null));
+  }, [dispatch]);
+
+  useEffect(() => {
+      console.log(quizzes);
+  }, [quizzes]);
+
+  return (
+    <div className="w-full h-full flex justify-center mt-20">
+      <div className="w-3/5 h-full">
+          <h1 className="text-black text-6xl mb-30">Available Quizzes</h1>
+        <div>
+          {quizzes.length === 0 ? (
+            <span className="text-black text-3xl text-center w-full flex justify-center">
+              No quizzes yet, add your first quiz.
+            </span>
+          ) : (
+            <QuizList />
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ src/pages/dashboard/components/student/components/QuizItem.jsx
@@ -0,0 +1,63 @@
+import { IconButton } from '@mui/material';
+import { Calendar, CircleUser, Clock, Play, SquarePen, Timer } from 'lucide-react';
+import { useDispatch } from 'react-redux';
+import { deleteQuiz } from '../../../../../redux/reducers/teacherSlice';
+import { formatDate } from '../../../../../utils/formatDate';
+
+export default function QuizItem({ quiz }) {
+  const dispatch = useDispatch();
+
+  const { date, time } = formatDate(quiz.startTime);
+
+  const quizStart = new Date(quiz.startTime).getTime();
+  const quizEnd = quizStart + quiz.durationInMinutes * 60 * 1000;
+  const now = Date.now();
+
+  return (
+    <div className="flex justify-center my-4">
+      <div className="flex flex-row justify-between items-center w-4/5 p-6 border-2 border-blue-200 rounded-2xl shadow-2xl shadow-cyan-200 bg-white">
+        <div className="flex flex-col w-11/12 space-y-4">
+          <h1 className="text-black text-3xl font-semibold">{quiz.title}</h1>
+          <div className="flex flex-row justify-between w-full text-black">
+            <div className="flex flex-row items-center space-x-2">
+              <SquarePen />
+              <span className="text-lg">{quiz.questions.length} Questions</span>
+            </div>
+            <div className="flex flex-row items-center space-x-2">
+              <Timer />
+              <span className="text-lg">{quiz.durationInMinutes} Minutes</span>
+            </div>
+            <div className="flex flex-row items-center space-x-2">
+              <CircleUser />
+              <span className="text-lg">Year {quiz.year}</span>
+            </div>
+            <div className="flex flex-row items-center space-x-2">
+              <Calendar />
+              <span className="text-lg">{date}</span>
+            </div>
+            <div className="flex flex-row items-center space-x-2">
+              <Clock />
+              <span className="text-lg">{time}</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex items-center justify-center">
+          <IconButton
+            sx={{
+              width: 48,
+              height: 48,
+              borderRadius: '50%',
+              padding: 0,
+            }}
+            // @ts-ignore
+            onClick={() => dispatch(deleteQuiz(quiz.id))}
+            disabled={now < quizStart || now > quizEnd}
+          >
+            <Play color="green" />
+          </IconButton>
+        </div>
+      </div>
+    </div>
+  );
+}
